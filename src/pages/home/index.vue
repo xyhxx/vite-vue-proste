e3285f32132681e20f21dd5ca130e78f570de7dc
xyh <603331487@qq.com>
2023-05-17 09:48:05 +0800

    chore: update eslint config

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup lang='ts'>
 import {useCountState, storeToRefs} from '@stores';
 
 defineOptions({
@@ -11,13 +11,13 @@ const {count} = storeToRefs(countState);
 
 <template>
   <main>
-    <img src='@assets/images/logo.png' class='icon' />
-    <h1 id='count' :class='["title", "name", {"title-red": count > 5}]'>
+    <img src="@assets/images/logo.png" class="icon" />
+    <h1 id="count" :class="['title','name', {'title-red': count > 5}]">
       count is {{count}}
     </h1>
-    <div class='btn-group'>
-      <button id='inc_btn' @click='countState.increment'>inc</button>
-      <button id='dec_btn' @click='countState.reduce'>dec</button>
+    <div class="btn-group">
+      <button id="inc_btn" @click="countState.increment">inc</button>
+      <button id="dec_btn" @click="countState.reduce">dec</button>
     </div>
   </main>
 </template>
