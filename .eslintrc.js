f19d3cf52304a0d807c777be239ccc1aa8f5ef55
xyhxx <603331487@qq.com>
2023-06-23 12:47:57 +0800

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'proste/vue',
     'proste/vitest',
   ],
+  plugins: ['xyhxx'],
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
@@ -11,6 +12,10 @@ module.exports = {
     tsconfigRootDir: '.',
     warnOnUnsupportedTypeScriptVersion: true,
     extraFileExtensions: ['.vue'],
+  },
+  rules: {
+    'xyhxx/if-line': 1,
+    'xyhxx/object-pattern-newline': 1,
   },
   overrides: [
     {
